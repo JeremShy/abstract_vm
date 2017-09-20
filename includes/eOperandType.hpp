b24ec69073b1dc411ea40c63d70e0456b1727d62
jcamhi <jeremshy@h3lp.fr>
2017-09-20 18:04:29 +0200

    Rainbow Dash is best pony !

---
+++ includes/eOperandType.hpp
@@ -0,0 +1,13 @@
+#ifndef EOPERANDTYPE
+# define EOPERANDTYPE
+
+namespace NOperandType {
+	typedef enum  {
+		Int8,
+		Int16,
+		Int32,
+		Float,
+		Double
+	} eOperandType;
+}
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stack>
+#include <eOperandType.hpp>
+#include <IOperand.hpp>
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+
+}
